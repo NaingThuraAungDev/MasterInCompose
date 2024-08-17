a8c695716cb27899fccecf5643b9ceb09b61ef2b
Naing Thura Aung <naingthuraaung2018@gmail.com>
2024-08-17 23:10:46 +0700

    add require import

--- BasicLayoutCompose/app/src/main/java/com/example/basiccompose/MainActivity.kt
+++ BasicLayoutCompose/app/src/main/java/com/example/basiccompose/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.basiccompose.ui.theme.BasicComposeTheme
 
 class MainActivity : ComponentActivity() {
